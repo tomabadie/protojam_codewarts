32dca812bc81698d95dfbae642f173d46ac0bf2c
Kaneb Florentin <Florentink.pro@gmail.com>
2025-05-22 15:04:59 +0200

    feat: redirection de la page 404 a la page home

--- src/components/Home/Error404.tsx
+++ src/components/Home/Error404.tsx
@@ -1,9 +1,20 @@
-function Error404() {
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+const Error404 = () => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		setTimeout(() => {
+			navigate("/");
+		}, 3000);
+	});
+
 	return (
 		<div>
 			<h1>404</h1>
 		</div>
 	);
-}
+};
 
 export default Error404;
